b05091f51683bca5171487f8036dcec99fe7e204
KilyongJeong <aceppin@motizensoft.com>
2021-07-05 16:18:09 +0900

    .

--- ch03/ex03-17.js
+++ ch03/ex03-17.js
@@ -1,13 +1,24 @@
 var arr = ['zero', 'one', 'two'];
-// var arr = ['zero', 'one', 'two', 'three', 'four'];
+var arr = ['zero', 'one', 'two', 'three', 'four'];
 console.log(arr.length);
 
-
 // 마지막 요소 접근
-
+var last = arr[arr.length-1];
+console.log('마지막 요소', last);
 
 // 마지막 요소 추출
-
+// var last = arr[arr.length-1];
+// arr.length--;
+var last = arr.pop();
+console.log('추출한 마지막 요소', last);
 
 // 첫번째 요소 추출
+// var first = arr[0];
+// for(var i=0; i<arr.length-1; i++){
+//   arr[i] = arr[i+1];
+// }
+// arr.length--;
+var first = arr.shift();
+console.log('추출한 첫번째 요소', first);
 
+console.log(arr);
