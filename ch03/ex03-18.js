fb8454445fcc814fe23a695f42df2b2e1b0f9adf
KilyongJeong <aceppin@motizensoft.com>
2021-07-05 16:45:03 +0900

    .

--- ch03/ex03-18.js
+++ ch03/ex03-18.js
@@ -1,7 +1,15 @@
 // 지정한 배열의 모든 요소를 출력한다.
 function printArr(arr){
-	
+	for(var i=0; i<arr.length; i++){
+    console.log(i, arr[i]);
+  }
 }
 
 var colorArr = ['orange', 'yellow', 'green'];
 printArr(colorArr);
+
+// 유사배열객체
+var colorObj = {
+  
+};
+printArr(colorObj);
