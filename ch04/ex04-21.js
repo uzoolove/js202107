a5d2c413fadd5ae1a540af1e5217812a50441791
KilyongJeong <aceppin@motizensoft.com>
2021-07-06 13:52:54 +0900

    .

--- ch04/ex04-21.js
+++ ch04/ex04-21.js
@@ -1,5 +1,11 @@
 function fn(n1, n2){
-  console.log(n1, n2);
+  // console.log(n1, n2);
+  // 모든 인자값의 합계를 출력
+  var sum = 0;
+  for(var i=0; i<arguments.length; i++){
+    sum += arguments[i];
+  }
+  console.log('합계', sum);
 }
 
 fn();
