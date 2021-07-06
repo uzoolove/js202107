0fd5a8af7d0df6878ec6d1d75fa7de078133fd8d
KilyongJeong <aceppin@motizensoft.com>
2021-07-06 13:42:55 +0900

    .

--- ch04/ex04-01.js
+++ ch04/ex04-01.js
@@ -1,4 +1,6 @@
 // 선언문 방식의 함수 선언
-
+function add(x, y){
+  return x + y;
+}
 
 console.log(add(10, 20));
--- etc/localvariablescope.js
+++ etc/localvariablescope.js
@@ -1,12 +1,13 @@
 // 지정한 두 값의 최대값을 반환한다.
 function max(n1, n2){
+  // TODO: hoisting 단원.
 	// var maxNum;
-  let maxNum;
+  // let maxNum;
 	
   if(n1 > n2){
-    maxNum = n1;
+    var maxNum = n1;
   }else{
-    maxNum = n2;
+    var maxNum = n2;
   }
 	
 	return maxNum;
