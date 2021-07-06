f3148604d84874e164544fec298a7833c380c23f
KilyongJeong <aceppin@motizensoft.com>
2021-07-06 09:50:24 +0900

    .

--- ch03/ex03-20.js
+++ ch03/ex03-20.js
@@ -6,5 +6,32 @@ function printArr(arr){
 	}
 }
 
-var arr = ['zero', 'one', 'two'];
+// 지정한 객체의 모든 속성을 출력한다.
+function printObj(obj){
+  for(var prop in obj){
+    if(obj.hasOwnProperty(prop)){
+      console.log(prop, obj[prop]);
+    }
+  }
+}
 
+var arr = ['zero', 'one', 'two'];
+arr.push('three');
+arr.push('four');
+arr.pop();
+arr.shift();
+
+// 일반 객체처럼 속성 추가
+arr.name = 'kim';
+
+// 대입연산자로 속성 추가.
+// arr.age = 30;
+
+// 속성 추가 전용 메소드
+Object.defineProperty(arr, 'age', {
+  value: 30,
+  enumerable: true, // 열거 가능한지 여부(for-in, Object.keys(), Object.values() ... 에 노출여부), 생략시 false
+  writable: false,  // 대입연산자로 value 수정이 가능한지 여부(상수형 변수), 생략시 false
+});
+
+printObj(arr);
