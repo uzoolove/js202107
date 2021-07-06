9882697296eba493d6ae88c23af0747e004fcde3
KilyongJeong <aceppin@motizensoft.com>
2021-07-06 15:54:35 +0900

    .

--- etc/smallest.js
+++ etc/smallest.js
@@ -4,7 +4,16 @@ console.log(Math.min(100,20,60,50,70)); // 20
 
 // 지정한 배열의 요소 중 최소값을 반환
 function smallest(nums){
-	
+	// var min = nums[0];
+  // for(var i=1; i<nums.length; i++){
+  //   if(nums[i] < min){
+  //     min = nums[i];
+  //   }
+  // }
+  // return min;
+  return Math.min.apply(this, nums);
+  // TODO: prototype 단원. min()은 prototype.js 라이브러리에서 추가한 배열 메소드
+  // return nums.min();
 }
 
 console.log(smallest([10,100]));	// 10
