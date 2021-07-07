6c489eeb239f3a662cdce03e9ae671dac417128f
KilyongJeong <aceppin@motizensoft.com>
2021-07-07 15:10:22 +0900

    .

--- etc/nodejs/httpserver.js
+++ etc/nodejs/httpserver.js
@@ -1,15 +1,11 @@
 const http = require('http');
 
-http.createServer(function(socket){
-  console.log(socket.remoteAddress, '접속');
-  socket.on('data', function(data){
-    console.log('from client:', data.toString());
-    // socket.write('hi');
-    socket.write('HTTP/1.1 200 OK\n');
-    socket.write('Content-Type: text/html;charset=utf-8\n');
-    socket.write('\n');
-    socket.end('<h1>안녕</h1>');
-  });
-}).listen(1234, function(){
-  console.log('TCP 서버 구동.');
+http.createServer(function(req, res){
+  console.log(req.method, req.url, req.httpVersion);
+  console.log(req.headers);
+
+  res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+  res.end('<h1>안녕</h1>');  
+}).listen(80, function(){
+  console.log('HTTP 서버 구동.');
 });
