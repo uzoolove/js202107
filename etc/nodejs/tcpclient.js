81c4ef621a732d88660ba50c345a3bdd233ebbf5
KilyongJeong <aceppin@motizensoft.com>
2021-07-07 14:50:19 +0900

    .

--- etc/nodejs/tcpclient.js
+++ etc/nodejs/tcpclient.js
@@ -1,7 +1,9 @@
 const net = require('net');
 
-var socket = net.createConnection(1234, 'localhost');
+// var socket = net.createConnection(1234, 'localhost');
+var socket = net.createConnection(80, 'google.com');
 socket.on('data', function(data){
   console.log('from server:', data.toString());
 });
-socket.write('hello');
+// socket.write('hello');
+socket.write('GET / HTTP/1.1\n\n');
--- etc/nodejs/tcpserver.js
+++ etc/nodejs/tcpserver.js
@@ -4,7 +4,11 @@ net.createServer(function(socket){
   console.log(socket.remoteAddress, '접속');
   socket.on('data', function(data){
     console.log('from client:', data.toString());
-    socket.write('hi');
+    // socket.write('hi');
+    socket.write('HTTP/1.1 200 OK\n');
+    socket.write('Content-Type: text/html;charset=utf-8\n');
+    socket.write('\n');
+    socket.end('<h1>안녕</h1>');
   });
 }).listen(1234, function(){
   console.log('TCP 서버 구동.');
