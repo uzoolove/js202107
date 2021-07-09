63c77d4a3f3d4ae23911279110764604cc7e6c06
KilyongJeong <aceppin@motizensoft.com>
2021-07-09 10:48:42 +0900

    .

--- ch06/ex06-09.js
+++ ch06/ex06-09.js
@@ -1,15 +1,25 @@
 var Counter = function(){
-	this.count = 0;
+	var count = 0;
 	this.ride = function(){
-		this.count++;
+    if(count < 40){
+      count++;
+    }else{
+      console.log('정원 초과!');
+    }
 	};
 	this.getCount = function(){
-		return this.count;
+		return count;
 	};
 };
 
 var c = new Counter();
 c.ride();
 c.ride();
+
+// c.count += 40;
+
+for(var i=0; i<40; i++){
+  c.ride();
+}
 
 console.log('전체 탑승자', c.getCount());
