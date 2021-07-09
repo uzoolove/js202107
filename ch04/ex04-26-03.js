b559d754b4c930e56504ff8b47059f4bb5ae7803
KilyongJeong <aceppin@motizensoft.com>
2021-07-09 13:35:50 +0900

    .

--- ch04/ex04-26-03.js
+++ ch04/ex04-26-03.js
@@ -1,1 +1,26 @@
 // ex04-26.js 복사
+var count = 0;
+var myObj = {
+	count: 0,
+	visit: function(){
+		// 방문자를 한명 증가시킨다.
+		this.count++; // this = myObj
+    var visitN = function(n){
+      this.count += n; // this = myObj
+    };
+    visitN.call(this, 2);
+    visitN.call(this, 2);
+
+    var visit3 = visitN.bind(this, 3);
+    visit3();
+    visit3();
+
+    var visit5 = visitN.bind(this, 5);
+    visit5();
+    visit5();
+	}
+};
+
+myObj.visit();
+myObj.visit();
+console.log('합계', myObj.count);
